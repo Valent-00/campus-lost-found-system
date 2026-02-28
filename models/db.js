bb1fc74c0352a40768a6f4839b0e5a9c14432d33
Valent <jiashiuhchua0902@gmail.com>
2026-02-28 20:28:34 +0800

    Fix database connection parsing

--- models/db.js
+++ models/db.js
@@ -1,5 +1,16 @@
 const mysql = require("mysql2");
+require("dotenv").config();
 
-const db = mysql.createPool(process.env.DATABASE_URL);
+const url = new URL(process.env.DATABASE_URL);
+
+const db = mysql.createPool({
+  host: url.hostname,
+  user: url.username,
+  password: url.password,
+  database: url.pathname.replace("/", ""),
+  port: url.port,
+  waitForConnections: true,
+  connectionLimit: 10,
+});
 
 module.exports = db.promise();
